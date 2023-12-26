d106afeb8f8b193d92bb7238d052985043d8c8c5
Richard <richrd77@gmail.com>
2023-12-27 01:28:31 +0530

    some finishing touches

--- src/app/components/info/info.component.ts
+++ src/app/components/info/info.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface qualification {
+  deg: string;
+  uni: string;
+  tim: string;
+}
+
 @Component({
   selector: 'ric-info',
   templateUrl: './info.component.html',
@@ -9,5 +15,11 @@ export class InfoComponent {
 
   public skills: string[] = [
     'C#', 'dotnet core', 'angular', 'Entity Framework', 'HTML', 'JavaScript', 'CSS', 'Containers'
+  ];
+
+  public qualifications: qualification[] = [
+    { deg: 'Bachelors in Engineering (EEE)', uni: 'PES Inistitute of Technology', tim: '2008-2012' },
+    { deg: 'Pre-university (Science)', uni: 'Shri Duttagiri Maharaj', tim: '2006-2008' },
+    { deg: 'Higher Secondary Education', uni: 'Seventh-day adventist', tim: '2006' },
   ];
 }
